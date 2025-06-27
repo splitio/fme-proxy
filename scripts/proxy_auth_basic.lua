5fc76c09bd9a6b4c73f29a8ab183ceb9a4624a72
Martin Redolatti <martin.redolatti@harness.io>
2025-06-27 16:54:31 -0300

    autogenerated config

--- scripts/proxy_auth_basic.lua
+++
@@ -1,6 +0,0 @@
-if not ngx.var.http_proxy_authorization then
-    ngx.header["Proxy-Authenticate"] = "Basic realm=\"Access to internal site\""
-    ngx.exit(407)
-end
-
-ngx.req.set_header("Authorization", ngx.var.http_proxy_authorization)
--- scripts/proxy_auth_digest.lua
+++
@@ -1,8 +0,0 @@
--- digest auth relies on a retry with proper credentials on a second request
--- over the same TCP connection.
--- Hence we cannot reject the first one for a lack of `proxy-authentication header`
--- Once we get that header we map it to `Authorization` so that the plugin can
--- properly handle it
-if ngx.var.http_proxy_authorization then
-    ngx.req.set_header("Authorization", ngx.var.http_proxy_authorization)
-end
