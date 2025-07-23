e8c29d26090df7a0b733f1802ad8f3fdcdea9d69
Martin Redolatti <martinredolatti@gmail.com>
2025-07-23 13:39:23 -0300

    more logging

--- scripts/genconf.sh
+++ scripts/genconf.sh
@@ -113,6 +113,10 @@ read -r -d '' TARGET_WHITELIST_BLOCK << "EOF"
         access_by_lua_block {
 
             local target = ngx.req.get_headers()["Host"]
+            ngx.log(ngx.STDERR, 'Incoming host header: <' .. target .. '>')
+            if ngx.var.request_uri then
+                ngx.log(ngx.STDERR, 'Request uri: <' .. ngx.var.request_uri .. '>')
+            end
             if string.find(target, ':') == nil then
                 target = target .. "80"
             end
