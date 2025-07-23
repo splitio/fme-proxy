9001f81f1ca8f43f6b8d6ac3f911bc9020dde892
Martin Redolatti <martinredolatti@gmail.com>
2025-07-23 19:26:55 -0300

    handle malformed host header

--- scripts/genconf.sh
+++ scripts/genconf.sh
@@ -114,7 +114,9 @@ read -r -d '' TARGET_WHITELIST_BLOCK << "EOF"
 
             local target = ngx.req.get_headers()["Host"]
             if string.find(target, ':') == nil then
-                target = target .. ":80"
+                local raw_headers = ngx.req.raw_header()
+                local request_line = string.sub(raw_headers, 0, string.find(raw_headers), "\r\n")
+                target = string.match(request_line, "CONNECT (.*:%d+) .*")
             end
 
             local allowed_hosts = require "host_whitelist_{{NAME}}".whitelist
